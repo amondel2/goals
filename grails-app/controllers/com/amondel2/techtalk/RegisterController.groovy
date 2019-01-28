59176b7322e08fa4ab9a7a6eedba805396859cc5
aaron <amondel2@gmail.com>
2019-01-27 23:11:12 -0500

    Fix a bug with Reset Token not being set and throwing an NPE

--- grails-app/controllers/com/amondel2/techtalk/RegisterController.groovy
+++ grails-app/controllers/com/amondel2/techtalk/RegisterController.groovy
@@ -124,7 +124,7 @@ class RegisterController extends grails.plugin.springsecurity.ui.RegisterControl
 				flash.message = "Profile Not Completed..Please contanct Site Admin for help"
 				redirect(controller: "login", action: "auth")
 			}
-			if(params.question1.trim().size() > 4 && params.question1.trim().toLowerCase() == qa[0].trim().toLowerCase() && params.question2.trim().toLowerCase() == qa[1].trim().toLowerCase() ) {
+			if(params.question1.trim().size() > 4 && params.question1.trim().toLowerCase() == qa[0]?.trim()?.toLowerCase() && params.question2.trim().toLowerCase() == qa[1]?.trim()?.toLowerCase() ) {
 				emp.restToken = null
 				emp.save(flush:true)
 				RegistrationCode registrationCode = registrationService.getForgotPassLink(emp)
